92633127a1cc7ca085a277d399ef38c80a23087c
fifteen1309 <txvitdev@gmail.com>
2024-05-30 16:10:22 +0700

    Use vercel/ncc

--- .github/actions/git-commit-info/src/index.ts
+++ .github/actions/git-commit-info/src/index.ts
@@ -3,12 +3,11 @@ const github = require('@actions/github')
 
 try {
   const nameToGreet = core.getInput('who-to-greet')
-  console.log("Heelo ", nameToGreet)
-  const time = (new Date()).toTimeString()
+  console.log('Heelo ', nameToGreet)
+  const time = new Date().toTimeString()
   core.setOutput('time', time)
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  
+  console.log(`The event payload: ${payload}`)
 } catch (e: any) {
   core.setFailed(e.message)
 }
