3555435c9d041b26489393cd633d5fdea2613be1
fifteen1309 <txvitdev@gmail.com>
2024-05-30 15:56:56 +0700

    feat: test greet actions

--- .github/actions/git-commit-info/src/index.ts
+++ .github/actions/git-commit-info/src/index.ts
@@ -1,5 +1,14 @@
-function greet(): string {
-  return "Hello world";
-}
+const core = require('@actions/core')
+const github = require('@actions/github')
 
-greet();
+try {
+  const nameToGreet = core.getInput('who-to-greet')
+  console.log("Heelo ", nameToGreet)
+  const time = (new Date()).toTimeString()
+  core.setOutput('time', time)
+  const payload = JSON.stringify(github.context.payload, undefined, 2)
+  console.log(`The event payload: ${payload}`);
+  
+} catch (e: any) {
+  core.setFailed(e.message)
+}
